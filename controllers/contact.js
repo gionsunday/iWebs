b3757a797fa11506682839d8be36438b2d08b0bc
gionsunday <richardadaji@gmail.com>
2024-09-29 10:42:13 +0100

    update

--- controllers/contact.js
+++ controllers/contact.js
@@ -40,9 +40,9 @@ const newMessage = async (req, res) => {
             html: ` 
             <body style="  display:flex; padding:10px; height:100%; width:100%; background-color:#cff7f7;"> 
             <div style="text-align:left; min-height:100%; padding:20px"> 
-              <h1 style="color:white; text-align: center; padding: 10px; text-shadow:0px 11px 5px rgb(148, 132, 88);background: linear-gradient(105deg, rgba(91,104,235,1) 0%, rgba(40,225,253,1) 100%); border-radius: 10px;"> <strong>iWebs</strong> </h1> 
+              <h1 style="color:white; text-align: center; padding: 10px; border-radius:20px; text-shadow:0px 11px 5px rgb(148, 132, 88);background: linear-gradient(105deg, rgba(91,104,235,1) 0%, rgba(40,225,253,1) 100%); border-radius: 10px;"> <strong>iWebs</strong> </h1> 
              <h2 style="color:#afafaf">Hi <strong style="font-family:comic san ms, cursive;">${name}</strong></h2> 
-            <p style="padding:10px; font-family: comic san ms, cursive;  text-alig:left !important; color:#afafaf; background-color: inherit; font-weight:400">We got your message, we will respond accordingly. <br/> <br/>Thank you!</p> 
+            <p style="padding:10px;  text-alig:left !important; color:#000; background-color: inherit; font-weight:400">We got your message, we will respond accordingly. <br/> <br/>Thank you!</p> 
             <div style="color:white; text-align: center; padding: 10px;background: inherit; border-radius: 10px; display: flex; justify-content: space-evenly;"> 
                 <a href="https://iwebs.onrender.com" style="text-decoration: none; color: white;box-shadow: inset 1px 2px 15px black; margin-right:20px; border-radius: 10px; padding: 5px; background: linear-gradient(105deg, rgba(91,104,235,1) 0%, rgba(40,225,253,1) 100%);">Goto site</a> 
                 <a href="tel:07065469493" style="text-decoration: none; color: white; border-radius: 10px;box-shadow: inset 1px 2px 15px black; margin-left:20px; padding: 5px; background: linear-gradient(105deg, rgba(91,104,235,1) 0%, rgba(40,225,253,1) 100%);">Call Support</a> 
